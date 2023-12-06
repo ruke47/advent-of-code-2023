8c9aa9c7024fe6ce8bba2d1ca5b2071029243829
Andrew Rueckert <ruke47@users.noreply.github.com>
2023-12-05 23:03:47 -0800

    Added a method to solve in O(1), using quadratic formula

--- d06/src/main.rs
+++ d06/src/main.rs
@@ -3,6 +3,7 @@ use std::time::SystemTime;
 fn main() {
     part1();
     part2();
+    part3();
 }
 
 fn part1() {
@@ -26,6 +27,13 @@ fn part2() {
     println!("Part 2: {wins} in {duration} ms")
 }
 
+fn part3() {
+    let time: i64 = 41968894;
+    let distance_record: i64 = 214178911271055;
+
+    let wins = quadratic_your_wins(time, distance_record);
+    assert_eq!(wins, 30077773);
+}
 
 fn count_wins(time: i64, distance_record: i64) -> i32 {
     let mut wins = 0;
@@ -37,4 +45,13 @@ fn count_wins(time: i64, distance_record: i64) -> i32 {
         }
     }
     wins
+}
+
+fn quadratic_your_wins(time: i64, distance_record: i64) -> i32 {
+    let ftime = time as f64;
+    let fdistance_record = distance_record as f64;
+    let lower: f64 = (ftime - (ftime.powf(2.0) - 4.0 * fdistance_record).sqrt())/2.0;
+    let upper: f64 = (ftime + (ftime.powf(2.0) - 4.0 * fdistance_record).sqrt())/2.0;
+
+    (upper - lower).ceil() as i32
 }
